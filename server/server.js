67c3bc9586ca782d5fa30315ef9c3bb9cd59c74f
Nasirilahi <nasirilahi.ilahi2@yahoo.com>
2017-06-10 23:40:31 +0530

    warning issue

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server,{pingTimeout: 30000});
 
 server.listen(3000);
 io = io.listen(server);
